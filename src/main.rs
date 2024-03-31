db200729778eb57a42f3a051ed3eec8bbc8d46b4
Zack H <zphixon@gmail.com>
2024-03-31 11:20:53 -0400

    hmm

--- src/main.rs
+++ src/main.rs
@@ -138,6 +138,7 @@ impl Page {
         }}
       }}
     </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, minimal-ui">
   </head>
   <body>
     <h1>{}</h1>
